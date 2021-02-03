b88a7cb54eed34bae4c68189ff7604aa2cd3677d
Manaswi Patil <manaswi.c.patil@gmail.com>
2021-02-03 14:03:15 +1300

    Initial upload

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using Beginner_Exercises.Arrays_and_Lists;
+using Beginner_Exercises.Control_Flow;
+using Beginner_Exercises.Working_with_Files;
+using Beginner_Exercises.Working_With_Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beginner_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*ValidNumber.Run();*/
+
+            //MaximumofTwo.Run();
+
+            //PortraitLandscapeImage.Run();
+
+            //SpeedCamera.Run();
+
+            //RandomPassword.Run();
+
+            //DivisiblebyThree.Run();
+
+            //SumofAllNumbers.Run();
+
+            //Factorial.Run();
+
+            //GuessTheNumber.Run();
+
+            //MaxNumberfromList.Run();
+
+            //LikesonthePost.Run();
+
+            //ReverseArray.Run();
+
+            //SortNumbers.Run();
+
+            //UniqueNumbers.Run();
+
+            //SmallestNumberinList.Run();
+
+            //StringPractice.Run();
+
+            //SummarisingText.Run();
+
+            //StringBuilderPractice.Run();
+
+            //ConsecutiveNumbers.Run();
+
+            //DuplicateNumber.Run();
+
+            //TimeFormatValidation.Run();
+
+            //PascalCase.Run();
+
+            //CountVowels.Run();
+
+            ReadFile.Run();
+
+            
+        }
+    }
+}
