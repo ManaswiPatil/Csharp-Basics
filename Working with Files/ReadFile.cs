e55274298f258e5a1f919c8364f132d760688030
Manaswi Patil <manaswi.c.patil@gmail.com>
2021-02-03 14:05:25 +1300

    Demo for Working with .txt files.

---
+++ Working with Files/ReadFile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beginner_Exercises.Working_with_Files
+{
+    class ReadFile
+    {
+        public static void Run()
+        {
+            var path = @"D:\Data Science\ASP.NET\Exercises\Beginner Exercises\Working with Files\Sample.txt";
+            // File.Create(path);
+
+            string text = File.ReadAllText(path);
+            Console.WriteLine(text);
+            Console.WriteLine();
+            var words = text.Split(' ');
+            var count = words.Length;
+            Console.WriteLine("Number of words in the file:" + count);
+
+            var max = 0;
+            var longestWord = "";
+            foreach(var word in words)
+            {
+                if (word.Length > max)
+                {
+                    max = word.Length;
+                    longestWord = word;
+                }
+
+            }
+
+            Console.WriteLine("Longest Word in the file is:" + longestWord);
+        }
+    }
+}
